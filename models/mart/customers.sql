039ab5c2576c9c6de29e07aaab5ded97f7cfa659
Ilahou <ilan.houssepian@gmail.com>
2026-04-26 15:20:57 +0200

    Create customers.sql to define dim_customers table

---
+++ models/mart/customers.sql
@@ -0,0 +1,4 @@
+CREATE OR REPLACE TABLE `converteo-ae-recrut-houssepian.Dashboard_Sales_ProductCat_Dates.dim_customers` AS
+SELECT
+ *
+FROM `converteo-ae-recrut-houssepian.Dashboard_Sales_ProductCat_Dates.stg_customers`
